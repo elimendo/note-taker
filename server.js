922186e6bb2e6a55245723b8e3e69b43e3dfc54a
elimendo <emendoza8361@gmail.com>
2024-01-29 18:15:20 -0800

    server up and running

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
